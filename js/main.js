d30157ce51d1228adb789516aea04b44e3c71ac0
fcprime <serhiinorov110@gmail.com>
2022-08-11 13:01:29 +0200

    next project

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function () {
+
+  $(document).ready(function () {
+    $(".menu a, .go-top").on("click", function (e) {
+      //отменяем стандартную обработку нажатия по ссылке
+      e  .preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+      var id = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({
+        scrollTop: top
+      }, 2000);
+    });
+  });
+
+  $('.slider-blog__inner').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
+
+  $('.menu__btn, .menu a').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+    $('.header__top-inner').toggleClass('header__top-inner--active');
+
+  });
+
+  var mixer = mixitup('.portfolio__content');
+
+});
